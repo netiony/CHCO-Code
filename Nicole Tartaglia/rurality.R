0e82d2605a7d24e85c81a0bcdd568404d5501a1f
laurapyle <laurapyle@gmail.com>
2021-10-21 20:54:44 -0600

    Create rurality

---
+++ Nicole Tartaglia/rurality.R
@@ -0,0 +1,3 @@
+babydata <- read.csv("H:\\Tartaglia\\Rurality\\EXtraordinarYBabyStu-ZipCodesRurality_DATA_2021-10-20_1602.csv")
+
+ruca <- read.csv("H:\\Tartaglia\\Rurality\\AP RUCA Codes - Pops 07.18.2019 - Copy.csv")
