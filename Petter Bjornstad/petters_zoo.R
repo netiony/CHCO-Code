8e1b44eff18df7bf571d024a9bef37bab11c1ff6
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-02-18 12:24:54 -0700

    Update petters_zoo.R

--- Petter Bjornstad/petters_zoo.R
+++ Petter Bjornstad/petters_zoo.R
@@ -1,1 +1,12 @@
 # Libraries
+library(tidyverse)
+library(redcapAPI)
+# Import api tokens
+
+# RENAL HEIR
+renal_heir = read.csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/Tim and Casey/RENALHEIR_DATA_2022-02-18_1154.csv")
+renal_heir = renal_heir %>% 
+  select(mr_number,dob,diagnosis,gender,scr) %>%
+  rename(mrn = mr_number,sex = gender,diagnosis_date = diagnosis)
+renal_heir$sex = factor(renal_heir$sex,levels = 0:2,labels = c("Male","Female","Other"))
+
