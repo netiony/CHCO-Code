0e4339e5b3d2185d84aae12c373a57b80645cc93
laurapyle <laurapyle@gmail.com>
2022-03-29 15:01:42 -0600

    linking IDs

--- Petter Bjornstad/TODAY proteomics metabolomics/make clinical data.R
+++ Petter Bjornstad/TODAY proteomics metabolomics/make clinical data.R
@@ -11,12 +11,6 @@ setwd(home_dir)
 # COMORB dataset
 comorb <- read.csv("./Clinical data/COMORB.csv")
 
-# read in the files that will link repository ID (column A) to Somalogic ID (column C)
-ids1 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at Colorado LEAD Center - Wash U.csv")
-ids2 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY - Wash U.csv")
-ids3 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY2 - Wash U.csv")
-ids <- rbind(ids1, ids2, ids3)
-
 # Save
 df = as.data.frame(comorb)
 save(comorb,file = "./comorb.Rdata")
