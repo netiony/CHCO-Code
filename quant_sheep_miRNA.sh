9ef1a7afc6d6a62def5a8c23cac0473ee9919953
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-05-27 11:11:31 -0600

    fastq cleaning mostly done

--- quant_sheep_miRNA.sh
+++ quant_sheep_miRNA.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# Move to folder
-cd /mnt/HD1/Tim/sheep_miRNA/
+# Move to base folder
+cd /mnt/HD1/Tim/sheep_miRNA
+# Copy fastq files to data folder
+find data_raw -name \*.fastq.gz -exec cp -n {} data_clean/fastq \;
+# Move to clean data folder
+cd data_clean
 # Index the sheep transcriptome
 salmon index -t Ovis_aries_rambouillet.Oar_rambouillet_v1.0.cdna.all.fa.gz -i ovis_index
 # Quantify reads
-for fn in data/DRR0161{25..40};
+for fn in $(find fastq/* -type f | sed 's/_R._001\..*//' | sort | uniq);
 do
 samp=`basename ${fn}`
+samp=${samp%_R1_001.fastq.gz}
+samp=${samp%_R2_001.fastq.gz}
 echo "Processing sample ${samp}"
-salmon quant -i athal_index -l A \
-         -1 ${fn}/${samp}_1.fastq.gz \
-         -2 ${fn}/${samp}_2.fastq.gz \
-         -p 12 --validateMappings --gcBias -o data_clean/${samp}_quant
+salmon quant -i ovis_index -l A \
+         -1 ${fn}/${samp}_R1_001.fastq.gz \
+         -2 ${fn}/${samp}_R2_001.fastq.gz \
+         -p 12 --validateMappings --gcBias -o quants/${samp}_quant
 done 
