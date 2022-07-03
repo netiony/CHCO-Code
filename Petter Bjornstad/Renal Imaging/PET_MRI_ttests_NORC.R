53183c7deb766d840cec66ce351d8673d9831940
laurapyle <laurapyle@gmail.com>
2022-07-02 18:15:35 -0600

    Create PET_MRI_ttests_NORC.R

---
+++ Petter Bjornstad/Renal Imaging/PET_MRI_ttests_NORC.R
@@ -0,0 +1,14 @@
+library(BSDA)
+
+tsum.test(
+  mean.x = 0.2444,
+  s.x = 0.027,
+  n.x = NULL,
+  mean.y = 0.3106,
+  s.y = 0.0287,
+  n.y = NULL,
+  alternative = "two.sided",
+  mu = 0,
+  var.equal = FALSE,
+  conf.level = 0.95
+)
