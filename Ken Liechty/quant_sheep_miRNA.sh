c4b9298cddf5399c37bffc2b2b41936be9f5d028
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-07-07 14:41:39 -0600

    better genome reference

--- Ken Liechty/quant_sheep_miRNA.sh
+++ Ken Liechty/quant_sheep_miRNA.sh
@@ -2,9 +2,9 @@
 # Move to base folder
 cd /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Vigers/sheep_miRNA
 # List of genome targets (decoys)
-grep "^>" <(gunzip -c data_clean/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa.gz) | cut -d " " -f 1 >data_clean/decoys.txt
+grep "^>" <(gunzip -c data_clean/Ovis_aries.Oar_v3.1.dna.toplevel.fa.gz) | cut -d " " -f 1 >data_clean/decoys.txt
 sed -i.bak -e 's/>//g' data_clean/decoys.txt
-cat data_clean/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.cdna.all.fa.gz data_clean/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa.gz >data_clean/gentrome.fa.gz
+cat data_clean/Ovis_aries.Oar_v3.1.cdna.all.fa.gz data_clean/Ovis_aries.Oar_v3.1.dna.toplevel.fa.gz >data_clean/gentrome.fa.gz
 # Index the sheep transcriptome
 salmon index -t data_clean/gentrome.fa.gz -d data_clean/decoys.txt -p 24 -i data_clean/ovis_index
 # Quantify reads
