9ad7efd21c18b484ed21eb744f0c392624eff7f9
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-07-14 15:34:33 -0600

    trim galore first run

--- Ken Liechty/quant_sheep_miRNA.sh
+++ Ken Liechty/quant_sheep_miRNA.sh
@@ -3,5 +3,19 @@
 cd ~/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Vigers/sheep_miRNA
 # QC
 for file in data_clean/FASTQ/*; do
-    fastqc ${file} -o data_clean/QC
+    fastqc ${file} -t 16 -o data_clean/QC
+done
+# Combine
+multiqc data_clean/QC
+# Trim adapters
+cd data_clean/FASTQ
+for fn in $(find . -name "*_R1_001.fastq.gz" -type f | sed 's/_R._001\..*//' | sort | uniq); do
+    trim_galore --length 0 -cores 8 --paired ${fn}_R1_001.fastq.gz ${fn}_R2_001.fastq.gz
+done
+# Move to new folder
+cd ~/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Vigers/sheep_miRNA
+find data_clean/FASTQ -name '*trimm*' -exec mv {} data_clean/FASTQ_trimmed \;
+# QC again
+for file in $(find data_clean/FASTQ_trimmed -name "*gz" -type f); do
+    fastqc ${file} -t 16 -o data_clean/QC_trimmed
 done
