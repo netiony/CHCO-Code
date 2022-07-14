2a91221e494d570c716315876280e4f91118cb59
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-07-14 14:15:09 -0600

    miRNA reference from miRBase

--- Ken Liechty/quant_sheep_miRNA.sh
+++ Ken Liechty/quant_sheep_miRNA.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 # Move to base folder
-cd /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Vigers/sheep_miRNA
-# # List of genome targets (decoys)
-# grep "^>" <(gunzip -c data_clean/Ovis_aries.Oar_v3.1.dna.toplevel.fa.gz) | cut -d " " -f 1 >data_clean/decoys.txt
-# sed -i.bak -e 's/>//g' data_clean/decoys.txt
-# cat data_clean/Ovis_aries.Oar_v3.1.cdna.all.fa.gz data_clean/Ovis_aries.Oar_v3.1.dna.toplevel.fa.gz >data_clean/gentrome.fa.gz
-# # Index the sheep transcriptome
-# salmon index -t data_clean/gentrome.fa.gz -d data_clean/decoys.txt -p 24 -i data_clean/ovis_index
-# Cut adapters
+cd ~/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Vigers/sheep_miRNA
+# QC
 for file in data_clean/FASTQ/*; do
-    samp=$(basename ${file})
-    echo "Cutting adapters from sample ${samp}"
-    cutadapt -j 24 -a TGGAATTCTCGGGTGCCAAGG data_clean/FASTQ/"$samp" -o data_clean/FASTQ_Trimmed/"$samp"
-done
-# Quantify reads
-for fn in $(find data_clean/FASTQ_Trimmed -name "*_R1_001.fastq.gz" -type f | sed 's/_R._001\..*//' | sort | uniq); do
-    samp=$(basename ${fn})
-    echo "Processing sample ${samp}"
-    salmon quant -i data_clean/ovis_index -l A \
-        -1 ${fn}_R1_001.fastq.gz \
-        -2 ${fn}_R2_001.fastq.gz \
-        -p 24 --validateMappings --gcBias -o data_clean/quants/${samp}_quant
+    fastqc ${file} -o data_clean/QC
 done
