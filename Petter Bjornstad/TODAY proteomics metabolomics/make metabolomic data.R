ec39d0bddaca97e7e5ae7189b93df2fb3b327879
laurapyle <laurapyle@gmail.com>
2022-07-28 14:55:52 -0600

    so sick of this

--- Petter Bjornstad/TODAY proteomics metabolomics/make metabolomic data.R
+++ Petter Bjornstad/TODAY proteomics metabolomics/make metabolomic data.R
@@ -54,9 +54,10 @@ nih_urine <- merge(nih_urine,ids_niddk,by="current_label",all.x = T, all.y = F)
 # for LEAD, need to merge MASK.ID in ID file to sample name in results file
 ids_lead$Sample.Name <- str_sub(ids_lead$MASK.ID, 1, 8)
 ids_lead$Sample.Name <- str_replace(ids_lead$Sample.Name, "-", "_")
-# below is not working...need to append "_U" to urine samples only, then merge lead urine results by Sample.Name
-# it's not working because c works on the whole set of sample.name, not each individual row
-ids_lead$Sample.Name <- ifelse(str_trim(ids_lead$material_type)=="Urine",c(ids_lead$Sample.Name,"_U"),ids_lead$Sample.Name)
+ids_lead$t <- ifelse(str_trim(ids_lead$material_type)=="Urine","_U","")
+ids_lead$Sample.Name <- paste(ids_lead$Sample.Name,ids_lead$t)
+ids_lead$t <- NULL
+lead_urine <- merge(lead_urine,ids_lead,by="Sample.Name",all.x=T, all.y = F)
 
 # merge to plasma
 plasma$current_label <- plasma$Freezerworks.ID
