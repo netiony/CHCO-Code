f913d0336f06341cf7ac60bd096aa45d6bcebf95
laurapyle <laurapyle@gmail.com>
2022-07-28 13:17:24 -0600

    more merge attempts

--- Petter Bjornstad/TODAY proteomics metabolomics/make metabolomic data.R
+++ Petter Bjornstad/TODAY proteomics metabolomics/make metabolomic data.R
@@ -23,9 +23,6 @@ nih_urine$site <- "NIH"
 lead_urine <- read.csv("./Metabolomic data/Lead_AA_20220322_20220620_Normalized_AF_urine.csv")
 lead_urine$site <- "LEAD"
 
-# merge
-urine <- rbind(nih_urine,lead_urine)
-
 ####################
 # plasma           #
 ####################
@@ -36,27 +33,29 @@ nih_plasma <- read.csv("./Metabolomic data/NIDDK_AA_20220427_20220620_Normalized
 # read in LEAD samples
 lead_plasma <- read.csv("./Metabolomic data/Lead_AA_20220322_20220620_Normalized_AF_plasma.csv")
 
-# merge
-plasma <- rbind(nih_plasma,lead_plasma)
 
 ######################
 # link IDs and merge #
 ######################
 
 # read in the files that will link repository ID to sample ID
-ids1 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at Colorado LEAD Center - UT Health San Antonio.csv")
-colnames(ids1) <- c("releaseid","material_type","current_label","MASK.ID","Date.Drawn","visnum","location")
-ids1$bsi_id <- NA
-ids2 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY - UT Health San Antonio.csv")
-ids2$MASK.ID <- NA
-ids3 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY2 - UT Health San Antonio.csv")
-ids3$MASK.ID <- NA
-ids <- rbind(ids1, ids2, ids3)
-ids$SampleDescription <- ids$current_label
+ids_lead <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at Colorado LEAD Center - UT Health San Antonio.csv")
+ids_lead$bsi_id <- NA
+ids_niddk_today <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY - UT Health San Antonio.csv")
+ids_niddk_today$MASK.ID <- NA
+ids_niddk_today2 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY2 - UT Health San Antonio.csv")
+ids_niddk_today2$MASK.ID <- NA
+ids_niddk <- rbind(ids_niddk_today, ids_niddk_today2)
 
 # merge to urine
-urine$current_label <- urine$Freezerworks.ID
-urine <- merge(urine,ids,by="current_label",all.x = T, all.y = F)
+# first NIH - merge Freezerworks ID in results file to current label in NIDDK file
+nih_urine$current_label <- nih_urine$Freezerworks.ID
+nih_urine <- merge(nih_urine,ids_niddk,by="current_label",all.x = T, all.y = F)
+# for LEAD, need to merge MASK.ID in ID file to sample name in results file
+ids_lead$Sample.Name <- str_sub(ids_lead$MASK.ID, 1, 8)
+ids_lead$Sample.Name <- str_replace(ids_lead$Sample.Name, "-", "_")
+# below is not working...need to append "_U" to urine samples only, then merge lead urine results by Sample.Name
+ids_lead$Sample.Name <- ifelse(str_trim(ids_lead$material_type)=="Urine",c(ids_lead$Sample.Name,"_U"),ids_lead$Sample.Name)
 
 # merge to plasma
 plasma$current_label <- plasma$Freezerworks.ID
