e977f629be60ac29ec4927359457f0335fab131b
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-08-18 15:48:13 -0600

    voxelwise data in rows

--- Petter Bjornstad/Data Harmonization/crocodile_voxelwise.R
+++
@@ -1,7 +0,0 @@
-library(R.matlab)
-# Import
-controls = readMat("/Users/timvigers/Dropbox/Work/Petter Bjornstad/CROCODILE/Data_Raw/IndivValuesVoxelWise/Controls.mat")
-t1d = readMat("/Users/timvigers/Dropbox/Work/Petter Bjornstad/CROCODILE/Data_Raw/IndivValuesVoxelWise/T1D.mat")
-# Format
-controls_indiv = unlist(controls[[3]],recursive = F)
-names(controls_indiv) = rep(rep(1:10,each = 4),2)
