327add500da9fac2bd7d5db10c611bfbbfd6b9fb
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-10-26 11:11:57 -0600

    Update michigan_sglt2i.R

--- Petter Bjornstad/Data Harmonization/exports/michigan_sglt2i.R
+++ Petter Bjornstad/Data Harmonization/exports/michigan_sglt2i.R
@@ -6,5 +6,15 @@ source("/Users/timvigers/GitHub/CHCO-Code/Petter Bjornstad/Data Harmonization/da
 df = harmonize_data()
 # Combine
 new_df = left_join(needed,df,by=c("Study ID"="subject_id","Visit"="visit"))
-# 
-check = new_df %>% select(`Study ID`,Visit,study,group,sglt2i)
+# Unique identifier for Michigan
+new_df$michigan_id = new_df$`Study ID`
+new_df$michigan_id[new_df$study == "IMPROVE" & new_df$Visit == "Baseline"] = 
+  paste0(new_df$michigan_id[new_df$study == "IMPROVE" & new_df$Visit == "Baseline"],"_BL")
+new_df$michigan_id[new_df$study == "IMPROVE" & new_df$Visit == "12 Months Post-Surgery"] = 
+  paste0(new_df$michigan_id[new_df$study == "IMPROVE" & new_df$Visit == "12 Months Post-Surgery"],"_12M")
+# Michigan ID first
+new_df = new_df %>% select(michigan_id,everything()) %>% select(-sglt2_inhibitors)
+# Write data
+write.csv(new_df,na = "",row.names = F,
+          file = paste0("~/Dropbox/Sample_ID_Master_Folder_CHCO_Petter/biopsy_clinical_",
+                        format(Sys.time(), "%Y_%m_%d_%H%M"),".csv"))
