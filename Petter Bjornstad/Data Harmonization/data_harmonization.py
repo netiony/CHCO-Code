7a14ab1794b6d7299d7c1b94f92345b33f34627c
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-11-09 15:02:54 -0700

    Update data_harmonization.py

--- Petter Bjornstad/Data Harmonization/data_harmonization.py
+++ Petter Bjornstad/Data Harmonization/data_harmonization.py
@@ -1,8 +1,8 @@
 """
-This code is designed to pull data from multiple REDCap projects and harmonize 
-the data in a single dataset. Some studies are cross-sectional but include 
-measures at multiple visits, and some studies are longitudinal. So, this code 
-outputs data in a "semi-long" format with one row per study procedure, and a 
+This code is designed to pull data from multiple REDCap projects and harmonize
+the data in a single dataset. Some studies are cross-sectional but include
+measures at multiple visits, and some studies are longitudinal. So, this code
+outputs data in a "semi-long" format with one row per study procedure, and a
 visit column for longitudinal clustering.
 """
 __author__ = "Tim Vigers"
@@ -14,13 +14,35 @@ __email__ = "timothy.vigers@cuanschutz.edu"
 __status__ = "Dev"
 
 # Libraries
+import sys
+import re
 import redcap
 import pandas as pd
-
+import numpy as np
+sys.path.insert(1,
+                "/Users/timvigers/GitHub/shared-resources/Data Cleaning/Data Manipulation in Python")
+from combine_redcap_checkboxes import combine_redcap_checkboxes
 # REDCap import variables
 tokens = pd.read_csv(
     "~/Documents/Work/CHCO/Petter Bjornstad/Data Harmonization/api_tokens.csv")
 uri = "https://redcap.ucdenver.edu/api/"
-
-# Import CROCODILE
-proj = redcap.Project(url=uri, your_rc_api_tkn)
+croc_token = tokens.loc[tokens["Study"] == "CROCODILE", "Token"].iloc[0]
+# For each cross-sectional study, import procedure forms separately then stack
+# longwise for "semi-long" data.
+# CROCODILE
+croc = redcap.Project(url=uri, token=croc_token)
+# Define demographic columns of interest
+dem_cols = ["record_id", "dob", "group", "sex", "race", "ethnicity"]
+# Export
+croc_demo = croc.export_records(format_type="df", fields=dem_cols)
+# Race columns combined into one
+croc_demo = combine_redcap_checkboxes(croc_demo, base_name="race",
+                                      levels=[
+                                          'American Indian/Alaska Native', 'Asian', 'Hawaiian/Pacific Islander', 'Black/African American', 'White', 'Other', 'Unknown'])
+# Same for ethnicity
+croc_demo = combine_redcap_checkboxes(croc_demo,
+                                      base_name="ethnicity",
+                                      levels=["Hispanic or Latino", "Not Hispanic or Latino", "Unknown/Not Reported"])
+# Relevel sex and group
+croc_demo["sex"].replace({1: "Male", 2: "Female", 3: "Other"}, inplace=True)
+croc_demo["group"].replace({1: "Type 1 Diabetes", 2: "Control"}, inplace=True)
