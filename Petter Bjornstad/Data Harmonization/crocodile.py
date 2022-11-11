8435f5a631cdaaae8d1f3b728815b43be89c34b1
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-11-11 09:00:05 -0700

    reorganize data harmonization

--- Petter Bjornstad/Data Harmonization/crocodile.py
+++ Petter Bjornstad/Data Harmonization/crocodile.py
@@ -1,9 +1,5 @@
 """
-This code is designed to pull data from multiple REDCap projects and harmonize
-the data in a single dataset. Some studies are cross-sectional but include
-measures at multiple visits, and some studies are longitudinal. So, this code
-outputs data in a "semi-long" format with one row per study procedure, and a
-visit column for longitudinal clustering.
+This code is designed to pull data from the CROCODILE REDCap project and output data in a "semi-long" format with one row per study procedure, and a visit column for longitudinal clustering when combined with other studies.
 """
 __author__ = "Tim Vigers"
 __credits__ = ["Tim Vigers"]
@@ -26,8 +22,8 @@ from combine_redcap_checkboxes import combine_redcap_checkboxes
 tokens = pd.read_csv(
     "~/Dropbox/Work/CHCO/Petter Bjornstad/Data Harmonization/api_tokens.csv")
 uri = "https://redcap.ucdenver.edu/api/"
-croc_token = tokens.loc[tokens["Study"] == "CROCODILE", "Token"].iloc[0]
-croc = redcap.Project(url=uri, token=croc_token)
+token = tokens.loc[tokens["Study"] == "CROCODILE", "Token"].iloc[0]
+proj = redcap.Project(url=uri, token=croc_token)
 
 # -----------------------------------------------------------------------------
 # For each cross-sectional study, import procedure forms separately then stack
@@ -37,35 +33,35 @@ croc = redcap.Project(url=uri, token=croc_token)
 # ------------------------------------------------------------------------------
 # CROCODILE
 # ------------------------------------------------------------------------------
-croc_meta = pd.DataFrame(croc.metadata)
-croc_df = croc.export_records(format_type="df")
+meta = pd.DataFrame(proj.metadata)
+df = proj.export_records(format_type="df")
 # DEMOGRAPHICS
 dem_cols = ["record_id", "dob", "diabetes_dx_date",
             "group", "sex", "race", "ethnicity"]
 # Export
-croc_demo = croc.export_records(format_type="df", fields=dem_cols)
+demo = proj.export_records(format_type="df", fields=dem_cols)
 # Race columns combined into one
-croc_demo = combine_redcap_checkboxes(croc_demo, base_name="race",
-                                      levels=[
-                                          'American Indian/Alaska Native', 'Asian', 'Hawaiian/Pacific Islander', 'Black/African American', 'White', 'Other', 'Unknown'])
+demo = combine_redcap_checkboxes(demo, base_name="race",
+                                 levels=[
+                                     'American Indian/Alaska Native', 'Asian', 'Hawaiian/Pacific Islander', 'Black/African American', 'White', 'Other', 'Unknown'])
 # Same for ethnicity
-croc_demo = combine_redcap_checkboxes(croc_demo,
-                                      base_name="ethnicity",
-                                      levels=["Hispanic or Latino", "Not Hispanic or Latino", "Unknown/Not Reported"])
+demo = combine_redcap_checkboxes(demo,
+                                 base_name="ethnicity",
+                                 levels=["Hispanic or Latino", "Not Hispanic or Latino", "Unknown/Not Reported"])
 # Relevel sex and group
-croc_demo["sex"].replace({1: "Male", 2: "Female", 3: "Other"}, inplace=True)
-croc_demo["group"].replace({1: "Type 1 Diabetes", 2: "Control"}, inplace=True)
+demo["sex"].replace({1: "Male", 2: "Female", 3: "Other"}, inplace=True)
+demo["group"].replace({1: "Type 1 Diabetes", 2: "Control"}, inplace=True)
 # PHYSICAL EXAM
-var = [v for v in croc_meta.loc[croc_meta["form_name"]
-                                == "physical_exam", "field_name"]]
-phys = croc_df.filter(var, axis=1)
+var = [v for v in meta.loc[meta["form_name"]
+                           == "physical_exam", "field_name"]]
+phys = df.filter(var, axis=1)
 phys["procedure"] = "physical_exam"
 phys.drop(["phys_normal", "phys_abnormal"], axis=1, inplace=True)
 phys.columns = phys.columns.str.replace(r"phys_", "")
 # SCREENING
-var = [v for v in croc_meta.loc[croc_meta["form_name"]
-                                == "screening_labs", "field_name"]]
-screen = croc_df.filter(var, axis=1)
+var = [v for v in meta.loc[meta["form_name"]
+                           == "screening_labs", "field_name"]]
+screen = df.filter(var, axis=1)
 screen.drop(["prescreen_a1c", "prescreen_a1c_date",
             "screen_menstrual", "screen_upt"], axis=1, inplace=True)
 screen.columns = screen.columns.str.replace(
