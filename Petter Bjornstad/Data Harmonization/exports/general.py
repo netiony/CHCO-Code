68924a5f377a95f0fffb3c63c113658adcc53661
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-12-06 14:13:06 -0700

    Create general.py

---
+++ Petter Bjornstad/Data Harmonization/exports/general.py
@@ -0,0 +1,23 @@
+"""
+This code is designed pulls our harmonized data and selects the variables Fadhl
+is using in his pre-/post-surgery analysis.
+"""
+__author__ = "Tim Vigers"
+__credits__ = ["Tim Vigers"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Tim Vigers"
+__email__ = "timothy.vigers@cuanschutz.edu"
+__status__ = "Dev"
+
+import os
+os.chdir(os.path.expanduser('~'))
+os.chdir("Documents/GitHub/CHCO-Code/Petter Bjornstad/Data Harmonization")
+import pandas as pd
+import numpy as np
+from data_harmonization import harmonize_data
+# Get dataset
+df = harmonize_data()
+df = df.loc[df["study"].isin(['CROCODILE', "IMPROVE"]), vars]
+# Write
+df.to_csv("~/Dropbox/Work/CHCO/Petter Bjornstad/Data Harmonization/Data Clean/harmonized_dataset.csv")
