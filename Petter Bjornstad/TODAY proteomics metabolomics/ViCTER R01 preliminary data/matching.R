092a5bc32d7e94b0ff61a554c437183919448786
laurapyle <laurapyle@gmail.com>
2022-12-08 18:02:30 -0700

    Create matching.R

---
+++ Petter Bjornstad/TODAY proteomics metabolomics/ViCTER R01 preliminary data/matching.R
@@ -0,0 +1,38 @@
+library(dplyr)
+
+if(Sys.info()["sysname"] == "Windows"){
+  home_dir = "E:/Petter Bjornstad/TODAY subaward"
+} else if (Sys.info()["sysname"] == "Linux"){
+  home_dir = "~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward"
+} else if (Sys.info()["sysname"] == "Darwin"){
+  home_dir = "/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward"
+}
+
+setwd(home_dir)
+
+# find those with normal kidney function at baseline
+comorb <- read.csv("./Clinical data/COMORB.csv")
+normal_kidney_function <- comorb %>% filter(MIC0==0 & MAC0==0 & NEPHRO0==0 & RAPID0==0)
+comorb$releaseid <- comorb$RELEASEID
+comorb$RELEASEID <- NULL
+
+# AGEBASE - uncollapsed age at baseline
+AGEBASE <- read.csv("./Clinical data/TODAY/AGEBASE.csv")
+AGEBASE$releaseid <- AGEBASE$RELEASEID
+AGEBASE$RELEASEID <- NULL
+for_matching <- merge(comorb, AGEBASE, by="releaseid",all.x = T,all.y = F)
+
+# PAT
+PAT <- read.csv("./Clinical data/TODAY/PAT.csv")
+keepPAT <- PAT %>% select(releaseid,sex)
+for_matching <- merge(for_matching,PAT,by="releaseid",all.x = T,all.y = F)
+
+# get treatment arm - PRIMOUT
+PRIMOUT <- read.csv("./Clinical data/TODAY/PRIMOUT.csv")
+PRIMOUT$releaseid <- PRIMOUT$PTID
+for_matching <- merge(for_matching,PRIMOUT,by="releaseid",all.x = T,all.y = F)
+# IDs in PRIMOUT don't seem to match the release IDs
+
+
+
+
