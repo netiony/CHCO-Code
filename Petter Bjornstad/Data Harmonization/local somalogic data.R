c1d45ca08948377906ae518114c3952d3a1d6164
Laura Pyle <laurapyle@gmail.com>
2023-01-09 18:07:53 -0700

    write study specific somalogic files

--- Petter Bjornstad/Data Harmonization/local somalogic data.R
+++ Petter Bjornstad/Data Harmonization/local somalogic data.R
@@ -1,52 +1,60 @@
 library(SomaDataIO)
+library(stringr)
 
 if(Sys.info()["sysname"] == "Windows"){
-  home_dir = "E:/Petter Bjornstad/TODAY subaward"
+  home_dir = "E:/Petter Bjornstad/Local cohort Somalogic data"
 } else if (Sys.info()["sysname"] == "Linux"){
-  home_dir = "~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/"
+  home_dir = "~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Local cohort Somalogic data/"
 } else if (Sys.info()["sysname"] == "Darwin"){
   home_dir = ""
 }
 setwd(home_dir)
 
 # read in data - we want to use the fully processed, normalized file ending in "anmlSMP.adat"
-soma <- read_adat("./Somalogic data raw/WUS-22-001_Somalogic_normalized/WUS-22-001_v4.1_EDTAPlasma.hybNorm.medNormInt.plateScale.calibrate.anmlQC.qcCheck.anmlSMP.adat")
+soma <- read_adat("./WUS-22-002_v4.1_EDTAPlasma_hybNorm_medNormInt_plateScale_calibrate_anmlQC_qcCheck_anmlSMP.adat")
 analytes <- getAnalyteInfo(soma)
 
-# read in the files that will link repository ID (column A) to Somalogic ID (column C)
-ids1 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at Colorado LEAD Center - Wash U.csv")
-colnames(ids1) <- c("releaseid","material_type","current_label","MASK.ID","Date.Drawn","visnum","location")
-ids1$bsi_id <- NA
-ids2 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY - Wash U.csv")
-ids2$MASK.ID <- NA
-ids3 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY2 - Wash U.csv")
-ids3$MASK.ID <- NA
-ids <- rbind(ids1, ids2, ids3)
-ids$SampleDescription <- ids$current_label
+# filter out Q/C samples
+soma <- soma %>% filter(!is.na(SampleDescription))
 
-# merge IDs with soma
-soma <- merge(soma,ids,by="SampleDescription",all.x = T,all.y = F)
+# create dataframes for each study
+# not sure which study IDs CKDSxxx belong to
+croc_soma <- soma %>% filter(str_sub(SampleDescription,1,3)=="CRC")
+improve_soma <- soma %>% filter(str_sub(SampleDescription,1,4)=="IT2D")
+rh_soma <- soma %>% filter(str_sub(SampleDescription,1,2)=="RH")
 
-# samples without a release ID are QC samples
-soma <- soma %>% filter(!is.na(releaseid))
+# Save CROCODILE
+if(Sys.info()["sysname"] == "Windows"){
+  dir = ""
+} else if (Sys.info()["sysname"] == "Linux"){
+  dir = "~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/CROCODILE/Somalogic data/"
+} else if (Sys.info()["sysname"] == "Darwin"){
+  dir = ""
+}
+setwd(dir)
+save(croc_soma,file = "./croc_soma.Rdata")
+save(analytes,file = "./analytes.Rdata")
 
-# fix date drawn
-soma$Date.Drawn <- as.Date(soma$Date.Drawn,format="%m/%d/%Y")
+# Save IMPROVE
+if(Sys.info()["sysname"] == "Windows"){
+  dir = ""
+} else if (Sys.info()["sysname"] == "Linux"){
+  dir = "~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/IMPROVE T2D/Somalogic data/"
+} else if (Sys.info()["sysname"] == "Darwin"){
+  dir = ""
+}
+setwd(dir)
+save(improve_soma,file = "./improve_soma.Rdata")
+save(analytes,file = "./analytes.Rdata")
 
-# merge in bariatric surgery data
-tme <- read.csv("./Clinical data/TODAY2/TME.csv")
-tme <- tme %>% filter(TMETYPE==1)
-tme <- tme %>% select(RELEASEID,TMETYPE,DAYSTOTME)
-colnames(tme) <- c("releaseid","TMETYPE","DAYSTOTME")
-soma <- merge(soma, tme, by="releaseid", all.x = T, all.y=F)
-# get rid of the 10 yr visit if DAYSTOTME<=3650
-# need to be sure to keep the baseline visit for those people
-# first add a variable that is sequential within ppt so we can tell baseline vs. followup
-soma <- soma %>% group_by(releaseid) %>% mutate(visit=seq_along(releaseid))
-soma <- soma %>% filter(!(visit==2 & !is.na(DAYSTOTME) & DAYSTOTME<3652))
-# drop bariatric surgery variables
-soma <- soma %>% select(-c(TMETYPE,DAYSTOTME))
-
-# Save
-save(soma,file = "./Somalogic data raw/soma.Rdata")
-save(analytes,file = "./Somalogic data raw/analytes.Rdata")
+# save RH
+if(Sys.info()["sysname"] == "Windows"){
+  dir = ""
+} else if (Sys.info()["sysname"] == "Linux"){
+  dir = "~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Renal HERITAGE/Somalogic data/"
+} else if (Sys.info()["sysname"] == "Darwin"){
+  dir = ""
+}
+setwd(dir)
+save(rh_soma,file = "./rh_soma.Rdata")
+save(analytes,file = "./analytes.Rdata")
