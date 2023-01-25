5154948809cf03f74a1dcc779accc394e7c1a714
laurapyle <laurapyle@gmail.com>
2023-01-24 18:00:44 -0700

    Create processing extra CGM files.R

---
+++ Christine Chan/Prospective analysis/processing extra CGM files.R
@@ -0,0 +1,4 @@
+library(cgmanalysis)
+
+cleandata(inputdirectory = "E:/Christine Chan/Prospective analysis/Data raw/Corrected outcome data/Extra CGM raw",
+          outputdirectory = "E:/Christine Chan/Prospective analysis/Data raw/Corrected outcome data/Extra CGM processed")
