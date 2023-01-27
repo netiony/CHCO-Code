dbbe0150b4df6d13c7d0708179ac42a69ebfa655
laurapyle <laurapyle@gmail.com>
2023-01-26 18:30:36 -0700

    Update processing extra CGM files.R

--- Christine Chan/Prospective analysis/processing extra CGM files.R
+++ Christine Chan/Prospective analysis/processing extra CGM files.R
@@ -1,4 +1,8 @@
 library(cgmanalysis)
 
+# need to get the new csv files in the right format - ID, date/time, glucose
+# read in the data and create date time variable
+
+
 cleandata(inputdirectory = "E:/Christine Chan/Prospective analysis/Data raw/Corrected outcome data/Extra CGM raw",
           outputdirectory = "E:/Christine Chan/Prospective analysis/Data raw/Corrected outcome data/Extra CGM processed")
