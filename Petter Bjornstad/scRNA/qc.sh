5509c6e7bf9642b84f039135cad86f82fff25690
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-02-06 06:33:53 -0700

    32 cores plus multiqc

--- Petter Bjornstad/scRNA/qc.sh
+++ Petter Bjornstad/scRNA/qc.sh
@@ -3,8 +3,7 @@ cd /mnt/HD2/Tim/scRNA
 # Run FastQC on all fastq files
 for file in $(find . -name "*.fastq.gz"); do
     SAMPLE=$(basename $file)
-    echo Data_Raw/${SAMPLE}
-    # fastqc -t 16 Data_Raw/${SAMPLE} -o /mnt/HD2/Tim/scRNA/Data_Clean/FastQC
+    fastqc -t 32 Data_Raw/${SAMPLE} -o /mnt/HD2/Tim/scRNA/Data_Clean/FastQC
 done
 # MultiQC to put everything together
-multiqc /Data_Clean/FastQC
+python3 -m multiqc Data_Clean/FastQC
