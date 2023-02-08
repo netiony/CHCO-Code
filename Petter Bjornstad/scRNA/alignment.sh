ac6cda7adf1fd2ef5efa078b2b3962d1977dcb71
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-02-08 10:34:22 -0700

    Update alignment.sh

--- Petter Bjornstad/scRNA/alignment.sh
+++ Petter Bjornstad/scRNA/alignment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /mnt/HD2/Tim/scRNA
+cd /Volumes/Work/scRNA
 # STARsolo for alignment (per Phil's recommendation)
+STAR --genomeDir /path/to/genome/dir/ --readFilesIn ...  --soloUMIlen 12 --soloType CB_UMI_Simple --soloCBwhitelist ~/GitHub/cellranger/lib/python/cellranger/barcodes/3M-february-2018.txt
