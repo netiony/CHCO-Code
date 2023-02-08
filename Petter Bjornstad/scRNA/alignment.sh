ff271116c9d5ef728aa1f2e92136b13dce7011c6
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-02-08 13:27:15 -0700

    Update alignment.sh

--- Petter Bjornstad/scRNA/alignment.sh
+++ Petter Bjornstad/scRNA/alignment.sh
@@ -5,7 +5,10 @@ cd /Volumes/Work/scRNA
 # Download reference data
 # wget https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-GRCh38-2020-A.tar.gz -P Miscellaneous
 # Use STAR to generate genome index
-STAR  --runMode genomeGenerate --runThreadN 4 --genomeDir Miscellaneous --genomeFastaFiles Miscellaneous/refdata-gex-GRCh38-2020-A/fasta/genome.fa  --sjdbGTFfile Miscellaneous/refdata-gex-GRCh38-2020-A/genes/genes.gtf
+STAR  --runMode genomeGenerate --runThreadN 4 \
+    --genomeDir Miscellaneous/indexed_reference \
+    --genomeFastaFiles Miscellaneous/refdata-gex-GRCh38-2020-A/fasta/genome.fa \
+    --sjdbGTFfile Miscellaneous/refdata-gex-GRCh38-2020-A/genes/genes.gtf
 # STARsolo for alignment (per Phil's recommendation)
 # See instructions at https://github.com/alexdobin/STAR/blob/master/docs/STARsolo.md
 STAR --genomeDir /path/to/genome/dir/ --readFilesIn ...  --soloUMIlen 12 --soloType CB_UMI_Simple --soloCBwhitelist ~/GitHub/cellranger/lib/python/cellranger/barcodes/3M-february-2018.txt
