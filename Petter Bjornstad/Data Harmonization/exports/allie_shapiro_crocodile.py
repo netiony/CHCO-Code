6e232b52fef3cb54ce61530d412d7559ab37b655
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-05-01 16:06:04 -0600

    CROC export for Allie done

--- Petter Bjornstad/Data Harmonization/exports/allie_shapiro_crocodile.py
+++ Petter Bjornstad/Data Harmonization/exports/allie_shapiro_crocodile.py
@@ -11,6 +11,7 @@ __status__ = "Dev"
 
 import os
 import sys
+import pandas as pd
 sys.path.insert(0, os.path.expanduser('~') +
                 "/GitHub/CHCO-Code/Petter Bjornstad/Data Harmonization")
 from data_harmonization import harmonize_data
@@ -18,18 +19,64 @@ from data_harmonization import harmonize_data
 df = harmonize_data()
 # Filter and select
 df = df[df["study"] == "CROCODILE"]
-
-mri = df[df["record_id"].isin(
-    ["CRC-10", "CRC-12", "CRC-13", "CRC-18", "CRC-20", "CRC-24", "CRC-28", "CRC-31", "CRC-33", "CRC-34", "CRC-35"])]
-mri = mri.groupby(by=["record_id"]).agg("last").reset_index()
+# Import neuro markers
+ab40 = pd.read_excel(
+    "/Volumes/Peds Endo/Petter Bjornstad/CROCODILE/Data_Cleaned/neuro_markers.xlsx",
+    sheet_name="Ab40 Results")
+ab40 = ab40[["record_id", 'AEB Rep1', 'AEB Rep2', 'Ave. AEB', 'CV',
+             'Conc. Rep1 (pg/mL)', 'Conc. Rep2 (pg/mL)', 'Ave. Conc. (pg/mL)',
+             'CV.1', 'Dilution Corrected Conc. (pg/mL)']]
+ab40.columns = [c + " AB40" if c != "record_id" else c for c in ab40.columns]
+ab42 = pd.read_excel(
+    "/Volumes/Peds Endo/Petter Bjornstad/CROCODILE/Data_Cleaned/neuro_markers.xlsx",
+    sheet_name="Ab42 Results")
+ab42 = ab42[["record_id", 'AEB Rep1', 'AEB Rep2', 'Ave. AEB', 'CV',
+             'Conc. Rep1 (pg/mL)', 'Conc. Rep2 (pg/mL)', 'Ave. Conc. (pg/mL)',
+             'CV.1', 'Dilution Corrected Conc. (pg/mL)']]
+ab42.columns = [c + " AB42" if c != "record_id" else c for c in ab42.columns]
+tau = pd.read_excel(
+    "/Volumes/Peds Endo/Petter Bjornstad/CROCODILE/Data_Cleaned/neuro_markers.xlsx",
+    sheet_name="Tau Results")
+tau = tau[["record_id", 'AEB Rep1', 'AEB Rep2', 'Ave. AEB', 'CV',
+           'Conc. Rep1 (pg/mL)', 'Conc. Rep2 (pg/mL)', 'Ave. Conc. (pg/mL)',
+           'CV.1', 'Dilution Corrected Conc. (pg/mL)']]
+tau.columns = [c + " Tau" if c != "record_id" else c for c in tau.columns]
+nfl = pd.read_excel(
+    "/Volumes/Peds Endo/Petter Bjornstad/CROCODILE/Data_Cleaned/neuro_markers.xlsx",
+    sheet_name="NFL Results")
+nfl = nfl[["record_id", 'AEB Rep1', 'AEB Rep2', 'Ave. AEB', 'CV',
+           'Conc. Rep1 (pg/mL)', 'Conc. Rep2 (pg/mL)', 'Ave. Conc. (pg/mL)',
+           'CV.1', 'Dilution Corrected Conc. (pg/mL)']]
+nfl.columns = [c + " NFL" if c != "record_id" else c for c in nfl.columns]
+gfap = pd.read_excel(
+    "/Volumes/Peds Endo/Petter Bjornstad/CROCODILE/Data_Cleaned/neuro_markers.xlsx",
+    sheet_name="GFAP Results")
+gfap = gfap[["record_id", 'AEB Rep1', 'AEB Rep2', 'Ave. AEB', 'CV',
+             'Conc. Rep1 (pg/mL)', 'Conc. Rep2 (pg/mL)', 'Ave. Conc. (pg/mL)',
+             'CV.1', 'Dilution Corrected Conc. (pg/mL)']]
+gfap.columns = [c + " GFAP" if c != "record_id" else c for c in gfap.columns]
+ptau181 = pd.read_excel(
+    "/Volumes/Peds Endo/Petter Bjornstad/CROCODILE/Data_Cleaned/neuro_markers.xlsx",
+    sheet_name="pTau 181 Results")
+ptau181 = ptau181[["record_id", 'AEB Rep1', 'AEB Rep2', 'Ave. AEB', 'CV',
+                   'Conc. Rep1 (pg/mL)', 'Conc. Rep2 (pg/mL)', 'Ave. Conc. (pg/mL)',
+                   'CV.1', 'Dilution Corrected Conc. (pg/mL)']]
+ptau181.columns = [c + " pTau 181" if c !=
+                   "record_id" else c for c in ptau181.columns]
 # Select variables of interest
-cols = ["record_id", "group", "age", "sex", "bmi",
-        'adc_left', 'adc_right', 'pasl2d_left', 'pasl2d_right', 'pcasl3d_left', 'pcasl3d_right', "volume_left", "volume_right"] + \
-    [c for c in mri.columns if "_bl_" in c] + \
-    [c for c in mri.columns if "_pf_" in c] + \
-    [c for c in mri.columns if "fsoc" in c] + \
-    ["rc_k1", "rc_k2", "rc_f", "rc_vb", "rm_k1", "rm_k2", "rm_f", "rm_vb",
-     "lc_k1", "lc_k2", "lc_f", "lc_vb", "lm_k1", "lm_k2", "lm_f", "lm_vb"]
-mri = mri[cols]
+cols = ["record_id", "group", "age", "diabetes_duration", "sex",
+        "bmi", "sbp", "dbp", "insulin_pump_timepoint",
+        "microalbumin_u", "p1_ffa_suppression", "p1_gc_leanm",
+        "p1_gc_m", "p1_raw_leanm", "p1_raw_m", "p2_gc_leanm", "p2_gc_m",
+        "p2_raw_leanm", "p2_raw_m"]
+df = df[cols]
+df = df.groupby(by=["record_id"]).agg("last").reset_index()
+# Merge
+df = pd.merge(df, ab40)
+df = pd.merge(df, ab42)
+df = pd.merge(df, tau)
+df = pd.merge(df, nfl)
+df = pd.merge(df, gfap)
+df = pd.merge(df, ptau181)
 # Write CSV
-mri.to_csv("~/mri_and_pet.csv", index=False)
+df.to_csv("~/crc_neuro_markers.csv", index=False)
