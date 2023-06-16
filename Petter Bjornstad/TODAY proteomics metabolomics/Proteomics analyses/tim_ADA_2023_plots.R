15294953bf6b234bfd8a912d2ef3e0419010e69d
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-06-15 21:02:08 -0600

    correct results file

--- Petter Bjornstad/TODAY proteomics metabolomics/Proteomics analyses/tim_ADA_2023_plots.R
+++ Petter Bjornstad/TODAY proteomics metabolomics/Proteomics analyses/tim_ADA_2023_plots.R
@@ -1,14 +1,16 @@
 library(tidyverse)
+library(ggrepel)
 library(readxl)
-library(forestplot)
+library(clusterProfiler)
+library(ReactomePA)
 # Import
-df = read_excel("/Volumes/Peds Endo/Petter Bjornstad/TODAY subaward/Results/TODAY proteomics Cox models scaled adjusted 2022-08-31.xlsx",sheet = "GLYC CPH")
-pCutoff = 0.001
+df = read_excel("/Volumes/Peds Endo/Petter Bjornstad/TODAY subaward/Results/Linear and Cox models/TODAY somalogic Cox models scaled baseline adjusted.xlsx",sheet = "GLYC CPH")
+df = df %>% arrange(p.value)
+pCutoff = 0.05
 # Volcano plot
 df$logp <- -log10(df$p.value)
-df$sig <- df$p.value <= pCutoff
+df$sig <- df$adj.p.value <= pCutoff
 vp = ggplot(df, aes(x = estimate, y = logp, color = sig)) +
-  geom_hline(yintercept = -log10(pCutoff), linetype = "dashed") +
   geom_point(size = 2) +
   geom_label_repel(
     data = df[df$sig, ], aes(label = TargetFullName), color = "black",
@@ -19,16 +21,22 @@ vp = ggplot(df, aes(x = estimate, y = logp, color = sig)) +
   ylab(bquote(~ -Log[10] ~ italic(P))) +
   theme_bw() +
   theme(legend.position = "none")
-ggsave("~/Documents/volcano.png",width = 1200,height = 900,units = "px",
-       plot = vp,scale = 2.5)
+ggsave("~/Documents/volcano.png",width = 1000,height = 900,units = "px",
+       plot = vp,scale = 2.25)
 # Forest plot
-df %>% filter(row_number() <= 10) %>%
+df %>% filter(adj.p.value <= pCutoff) %>%
   forestplot(labeltext = TargetFullName,
              mean = estimate,
              lower = conf.low,
              upper = conf.high,
              zero = 1,
-             cex  = 2,
-             lineheight = "auto",
-             xlab = "Lab axis txt")
-
+             xlab = "HR")
+# Pathway analysis
+gl = log(df$estimate[df$p.value <= 0.05])
+names(gl) = df$EntrezGeneID[df$p.value <= 0.05]
+gl = sort(gl,decreasing = T)
+gl = gl[!duplicated(names(gl))]
+gsea = gsePathway(gl,organism = "human")
+dp = enrichplot::dotplot(gsea)
+ggsave("~/Documents/dot.png",width = 1000,height = 900,units = "px",
+       plot = dp,scale = 1.8)
