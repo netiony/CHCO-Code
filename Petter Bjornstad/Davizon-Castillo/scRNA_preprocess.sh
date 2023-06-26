32c4244edc73a2024e01fde909f72ac29a14a712
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-06-26 10:15:20 -0600

    comment out unnecessary code

--- Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
+++ Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
@@ -8,12 +8,12 @@ for file in $(find Data_Raw/fastq -name "*.fastq.gz"); do
 done
 # MultiQC to put everything together
 python3 -m multiqc Data_Clean/QC/FastQC -o Data_Clean/QC --no-data-dir
-# Prepare STARsolo to mimic Cell Ranger as close as possible
-STAR  --runMode genomeGenerate \
-    --runThreadN 16 \
-    --genomeDir ./Miscellaneous/STAR \
-    --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/GRCh38.p13.genome.fa \
-    --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/gencode.v43.primary_assembly.annotation.gtf
+# Prepare STAR - no need to run every time, just want the code saved somewhere
+# STAR  --runMode genomeGenerate \
+#     --runThreadN 16 \
+#     --genomeDir /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/STAR \
+#     --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/GRCh38.p13.genome.fa \
+#     --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/gencode.v43.primary_assembly.annotation.gtf
 # Alignment with STARsolo
 STAR --genomeDir ./Miscellaneous/STAR \
     --readFilesIn $(find Data_Raw/fastqs/ -name "*_R1_*.fastq.gz" | tr '\n' ',') $(find Data_Raw/fastqs/ -name "*_R2_*.fastq.gz" | tr '\n' ',') \
