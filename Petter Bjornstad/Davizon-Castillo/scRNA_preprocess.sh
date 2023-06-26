9b17a2cddcd18d6894c276c97a5cc9b6a6276445
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-06-26 09:58:52 -0600

    STAR not STARsolo

--- Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
+++ Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
@@ -11,23 +11,12 @@ python3 -m multiqc Data_Clean/QC/FastQC -o Data_Clean/QC --no-data-dir
 # Prepare STARsolo to mimic Cell Ranger as close as possible
 STAR  --runMode genomeGenerate \
     --runThreadN 16 \
-    --genomeDir ./Miscellaneous/STAR\ Genome \
-    --genomeSAindexNbases 10 \
-    --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/scRNA/Miscellaneous/cellranger-7.1.0/external/cellranger_tiny_ref/fasta/genome.fa \
-    --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/scRNA/Miscellaneous/cellranger-7.1.0/external/cellranger_tiny_ref/genes/genes.gtf
+    --genomeDir ./Miscellaneous/STAR \
+    --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/GRCh38.p13.genome.fa \
+    --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/gencode.v43.primary_assembly.annotation.gtf
 # Alignment with STARsolo
-STAR --genomeDir ./Miscellaneous/STAR\ Genome \
-    --readFilesIn $(find Data_Raw/fastq/ -name "*_R1_*.fastq.gz" | tr '\n' ',') $(find Data_Raw/fastq/ -name "*_R2_*.fastq.gz" | tr '\n' ',') \
-    --soloType CB_UMI_Simple \
-    --soloCBwhitelist /home/tim/GitHub/cellranger/lib/python/cellranger/barcodes/737K-august-2016.txt \
-    --soloCBstart 1 \
-    --soloCBlen 16 \
-    --soloUMIstart 17 \
-    --soloUMIlen 10 \
-    --soloBarcodeMate 1 \
-    --clip5pNbases 39 0 \
+STAR --genomeDir ./Miscellaneous/STAR \
+    --readFilesIn $(find Data_Raw/fastqs/ -name "*_R1_*.fastq.gz" | tr '\n' ',') $(find Data_Raw/fastqs/ -name "*_R2_*.fastq.gz" | tr '\n' ',') \
     --readFilesCommand gunzip -c \
-    --genomeSAsparseD 3 \
-    --soloMultiMappers EM \
     --outFileNamePrefix ./Data_Clean/Mapped/\
     --runThreadN 16
