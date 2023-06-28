a0816fb93b4b1e9e1a93fad2d04b970a153eeec0
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-06-28 10:21:17 -0600

    one file at a time in a for loop

--- Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
+++ Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
@@ -11,19 +11,26 @@ cd /mnt/HD2/Davizon-Castillo
 # # MultiQC to put everything together
 # python3 -m multiqc data_clean/QC/FastQC -o data_clean/QC --no-data-dir
 
-# # Prepare STAR - no need to run every time, just want the code saved somewhere
+# # Prepare STAR genome to align to - no need to run every time, just want the code saved somewhere
 # STAR  --runMode genomeGenerate \
-#     --runThreadN 16 \
+#     --runThreadN 20 \
 #     --genomeDir /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/STAR \
 #     --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/GRCh38.p13.genome.fa \
 #     --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/gencode.v43.primary_assembly.annotation.gtf
 
-# Alignment with STARsolo
+# Load index into memory for faster processing
+STAR --genomeLoad LoadAndExit --genomeDir ./miscellaneous/STAR
+# Alignment with STAR
 for file in $(find data_raw/fastqs -name "*_R1_*.fastq.gz"); do
-    echo $file
+    name="$(basename -- $file)"
+    name=${name/_R1*/""}
     STAR --genomeDir ./miscellaneous/STAR \
+        --genomeLoad LoadAndKeep \
         --readFilesIn $file ${file/R1/"R2"} \
         --readFilesCommand gunzip -c \
-        --outFileNamePrefix ./data_clean/mapped/\
-        --runThreadN 16
+        --outFileNamePrefix "./data_clean/aligned/${name}"\
+        --quantMode GeneCounts \
+        --runThreadN 20
 done
+# Remove genome from memory
+STAR --genomeLoad Remove --genomeDir ./miscellaneous/STAR
