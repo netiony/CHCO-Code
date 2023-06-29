2896e474d65a45660f496cdcc36673e1b05617e3
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-06-28 18:11:09 -0600

    RSEM STAR run

--- Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
+++ Petter Bjornstad/Davizon-Castillo/scRNA_preprocess.sh
@@ -11,26 +11,19 @@ cd /mnt/HD2/Davizon-Castillo
 # # MultiQC to put everything together
 # python3 -m multiqc data_clean/QC/FastQC -o data_clean/QC --no-data-dir
 
-# # Prepare STAR genome to align to - no need to run every time, just want the code saved somewhere
-# STAR  --runMode genomeGenerate \
-#     --runThreadN 20 \
-#     --genomeDir /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/STAR \
-#     --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/GRCh38.p13.genome.fa \
-#     --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq/gencode.v43.primary_assembly.annotation.gtf
+# Prepare RSEM reference genome for alignment - no need to run every time, just want the code saved somewhere
+# cd /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/RNAseq
+# rsem-prepare-reference --star --star-path /home/tim/Tools/STAR/source -p 20 \
+#     --gtf gencode.v43.primary_assembly.annotation.gtf \
+#     GRCh38.p13.genome.fa \
+#     /mnt/HD2/Davizon-Castillo/reference_genome/ref
+# cd /mnt/HD2/Davizon-Castillo
 
-# Load index into memory for faster processing
-STAR --genomeLoad LoadAndExit --genomeDir ./miscellaneous/STAR
-# Alignment with STAR
-for file in $(find data_raw/fastqs -name "*_R1_*.fastq.gz"); do
-    name="$(basename -- $file)"
-    name=${name/_R1*/""}
-    STAR --genomeDir ./miscellaneous/STAR \
-        --genomeLoad LoadAndKeep \
-        --readFilesIn $file ${file/R1/"R2"} \
-        --readFilesCommand gunzip -c \
-        --outFileNamePrefix "./data_clean/aligned/${name}"\
-        --quantMode GeneCounts \
-        --runThreadN 20
-done
-# Remove genome from memory
-STAR --genomeLoad Remove --genomeDir ./miscellaneous/STAR
+# Calculate expression
+rsem-calculate-expression -p 20 \
+    --paired-end \
+	--star \
+    --star-path /home/tim/Tools/STAR/source \
+	data_raw/fastqs/0_1_cd93_3_1_S21_L002_R1_001.fastq.gz data_raw/fastqs/0_1_cd93_3_1_S21_L002_R2_001.fastq.gz \
+	/mnt/HD2/Davizon-Castillo/reference_genome \
+    data_clean/quals/test
