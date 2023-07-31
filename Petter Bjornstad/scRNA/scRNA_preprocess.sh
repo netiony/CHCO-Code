2eff0d9a482f58a2ffa318e0b7501b21bf7085ea
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-07-31 08:49:49 -0600

    fixed temp folder issue STAR

--- Petter Bjornstad/scRNA/scRNA_preprocess.sh
+++ Petter Bjornstad/scRNA/scRNA_preprocess.sh
@@ -7,17 +7,17 @@ for file in $(find data_raw/fastqs -name "*.fastq.gz"); do
     fastqc -t 16 data_raw/fastqs/${SAMPLE} -o data_clean/qc/fastqc
 done
 # MultiQC to put everything together
-python3 -m multiqc data_clean/qc/fastqc -o data_clean/qc --no-data-dir
-# Prepare STARsolo to mimic Cell Ranger as close as possible
-STAR  --runMode genomeGenerate \
-    --runThreadN 16 \
-    --genomeDir ./Miscellaneous/STAR\ Genome \
-    --genomeSAindexNbases 10 \
-    --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/scRNA/Miscellaneous/cellranger-7.1.0/external/cellranger_tiny_ref/fasta/genome.fa \
-    --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/scRNA/Miscellaneous/cellranger-7.1.0/external/cellranger_tiny_ref/genes/genes.gtf
+python3 -m multiqc data_clean/qc/fastqc -o data_clean/qc --no-data-dir --interactive
+# # Prepare STARsolo to mimic Cell Ranger as close as possible - don't need to run every time
+# STAR  --runMode genomeGenerate \
+#     --runThreadN 16 \
+#     --genomeDir ./Miscellaneous/STAR\ Genome \
+#     --genomeSAindexNbases 10 \
+#     --genomeFastaFiles /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/scRNA/Miscellaneous/cellranger-7.1.0/external/cellranger_tiny_ref/fasta/genome.fa \
+#     --sjdbGTFfile /home/tim/UCD/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/Peds\ Endo/Petter\ Bjornstad/scRNA/Miscellaneous/cellranger-7.1.0/external/cellranger_tiny_ref/genes/genes.gtf
 # Alignment with STARsolo
 STAR --genomeDir ./Miscellaneous/STAR\ Genome \
-    --readFilesIn $(find Data_Raw/fastq/ -name "*_R1_*.fastq.gz" | tr '\n' ',') $(find Data_Raw/fastq/ -name "*_R2_*.fastq.gz" | tr '\n' ',') \
+    --readFilesIn $(find data_raw/fastqs/ -name "*_R1_*.fastq.gz" | tr '\n' ',') $(find data_raw/fastqs/ -name "*_R2_*.fastq.gz" | tr '\n' ',') \
     --soloType CB_UMI_Simple \
     --soloCBwhitelist /home/tim/GitHub/cellranger/lib/python/cellranger/barcodes/737K-august-2016.txt \
     --soloCBstart 1 \
@@ -29,5 +29,6 @@ STAR --genomeDir ./Miscellaneous/STAR\ Genome \
     --readFilesCommand gunzip -c \
     --genomeSAsparseD 3 \
     --soloMultiMappers EM \
-    --outFileNamePrefix ./Data_Clean/Mapped/\
+    --outFileNamePrefix ./data_clean/mapped/ \
+    --outTmpDir /mnt/HD2/Bjornstad/scRNA/STAR_temp \
     --runThreadN 16
