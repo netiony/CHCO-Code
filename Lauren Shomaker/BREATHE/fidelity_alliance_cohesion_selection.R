0725f7b558c8b8fc75c050d0c6c10437e6c44346
laurapyle <laurapyle@gmail.com>
2023-10-31 17:04:43 -0600

    stratify by lead facilitator

--- Lauren Shomaker/BREATHE/fidelity_alliance_cohesion_selection.R
+++ Lauren Shomaker/BREATHE/fidelity_alliance_cohesion_selection.R
@@ -10,12 +10,12 @@ data$Lead_Cohort <- paste(data$`Lead Facilitator`,data$Cohort)
 
 # first select 30% of sessions for intervention fidelity, stratified by cohort 
 set.seed(3654)
-numvisits <- table(data$Lead_Cohort)
-numvisits_select <- floor(table(data$Lead_Cohort)*0.3)
+numvisits <- table(data$`Lead Facilitator`)
+numvisits_select <- floor(table(data$`Lead Facilitator`)*0.3)
 for (i in 1:8) {
   numvisits_select[i] <- ifelse(numvisits_select[i]==0, 1, numvisits_select[i])
 }
-temp <- strata(data=data, stratanames = "Lead_Cohort", size=numvisits_select, method="srswor")
+temp <- strata(data=data, stratanames = "Lead Facilitator", size=numvisits_select, method="srswor")
 res <- getdata(data, temp)
 res <- res[,c("Modality","Cohort","Session #","Lead Facilitator")]
 
@@ -24,12 +24,12 @@ write.csv(res,"/Volumes/Shared/Shared Projects/Laura/Peds Endo/Lauren Shomaker/B
 
 # then select 30% of cohorts 2-6 for alliance and cohesion, stratified by cohort and lead facilitator
 data_cohort26 <- data[data$Cohort>=2 & data$Cohort<=6,]
-numvisits <- table(data_cohort26$Lead_Cohort)
-numvisits_select <- floor(table(data_cohort26$Lead_Cohort)*0.3)
+numvisits <- table(data_cohort26$`Lead Facilitator`)
+numvisits_select <- floor(table(data_cohort26$`Lead Facilitator`)*0.3)
 for (i in 1:8) {
   numvisits_select[i] <- ifelse(numvisits_select[i]==0, 1, numvisits_select[i])
 }
-temp26 <- strata(data=data_cohort26, stratanames = "Lead_Cohort", size=numvisits_select, method="srswor")
+temp26 <- strata(data=data_cohort26, stratanames = "Lead Facilitator", size=numvisits_select, method="srswor")
 res26 <- getdata(data_cohort26, temp26)
 res26 <- res26[,c("Modality","Cohort","Session #","Lead Facilitator")]
 
