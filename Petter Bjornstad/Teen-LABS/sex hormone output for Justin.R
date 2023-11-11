01d274a24a0eb4a8ec32e25362ed4e68914926c4
laurapyle <laurapyle@gmail.com>
2023-11-11 12:25:27 -0700

    Create sex hormone output for Justin.R

---
+++ Petter Bjornstad/Teen-LABS/sex hormone output for Justin.R
@@ -0,0 +1,9 @@
+load("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Teen Labs/Data_Cleaned/analysis_dataset.RData")
+
+# keep only the proteins we need
+analyte_sh <- analyte_info %>% filter(UniProt %in% c("P04278","P01215|P01222","P01215|P01225","P01215|P01229"))
+apt_keep <- analyte_sh$AptName
+df_justin <- df %>% select(ID, visit, all_of(apt_keep))
+
+write.csv(df_justin, "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Teen Labs/soma_sex_hormone_Justin.csv",
+          row.names = F)
