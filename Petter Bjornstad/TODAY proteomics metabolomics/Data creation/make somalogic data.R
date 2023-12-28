df7f30da20b0acd1b4552a746497a126b3b015fa
laurapyle <laurapyle@gmail.com>
2023-12-27 20:05:18 -0700

    remove fc_mouse and no_protein

--- Petter Bjornstad/TODAY proteomics metabolomics/Data creation/make somalogic data.R
+++ Petter Bjornstad/TODAY proteomics metabolomics/Data creation/make somalogic data.R
@@ -7,25 +7,32 @@ if(Sys.info()["sysname"] == "Windows"){
 } else if (Sys.info()["sysname"] == "Darwin"){
   home_dir = ""
 }
-setwd(home_dir)
+#setwd(home_dir)
 
 # read in data - we want to use the fully processed, normalized file ending in "anmlSMP.adat"
 soma <- read_adat("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/Somalogic data raw/WUS-22-001_Somalogic_normalized/WUS-22-001_v4.1_EDTAPlasma.hybNorm.medNormInt.plateScale.calibrate.anmlQC.qcCheck.anmlSMP.adat")
 analytes <- getAnalyteInfo(soma)
 
+# remove fc mouse and no protein
+drop <- analytes %>% filter(Target == "Fc_MOUSE" | Target == "No Protein")
+apt_drop <- drop$AptName
+soma <- soma %>% select(!all_of(apt_drop))
+analytes <- analytes %>% filter(!Target == "Fc_MOUSE")
+analytes <- analytes %>% filter(!Target == "No Protein")
+
 # read in the files that will link repository ID (column A) to Somalogic ID (column C)
-ids1 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at Colorado LEAD Center - Wash U.csv")
+ids1 <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/Somalogic repository link/Omics-Petter Ancillary Samples at Colorado LEAD Center - Wash U.csv")
 colnames(ids1) <- c("releaseid","material_type","current_label","MASK.ID","Date.Drawn","visnum","location")
 ids1$bsi_id <- NA
-ids2 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY - Wash U.csv")
+ids2 <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY - Wash U.csv")
 ids2$MASK.ID <- NA
-ids3 <- read.csv("./Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY2 - Wash U.csv")
+ids3 <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/Somalogic repository link/Omics-Petter Ancillary Samples at NIDDK-TODAY2 - Wash U.csv")
 ids3$MASK.ID <- NA
 ids <- rbind(ids1, ids2, ids3)
 ids$SampleDescription <- ids$current_label
 
 # merge IDs with soma
-soma <- merge(soma,ids,by="SampleDescription",all.x = T,all.y = F)
+soma <- left_join(soma,ids,by="SampleDescription")
 
 # samples without a release ID are QC samples
 soma <- soma %>% filter(!is.na(releaseid))
@@ -34,19 +41,23 @@ soma <- soma %>% filter(!is.na(releaseid))
 soma$Date.Drawn <- as.Date(soma$Date.Drawn,format="%m/%d/%Y")
 
 # merge in bariatric surgery data
-tme <- read.csv("./Clinical data/TODAY2/TME.csv")
+tme <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/Clinical data/TODAY2/TME.csv")
 tme <- tme %>% filter(TMETYPE==1)
 tme <- tme %>% select(RELEASEID,TMETYPE,DAYSTOTME)
 colnames(tme) <- c("releaseid","TMETYPE","DAYSTOTME")
-soma <- merge(soma, tme, by="releaseid", all.x = T, all.y=F)
+soma <- left_join(soma, tme, by="releaseid")
 # get rid of the 10 yr visit if DAYSTOTME<=3650
 # need to be sure to keep the baseline visit for those people
 # first add a variable that is sequential within ppt so we can tell baseline vs. followup
-soma <- soma %>% group_by(releaseid) %>% mutate(visit=seq_along(releaseid))
+temp <- as.data.frame(soma[,"releaseid"])
+names(temp) <- "releaseid"
+temp <- temp %>% group_by(releaseid) 
+temp <- temp %>% mutate(visit=seq_along(releaseid)) 
+soma$visit <- temp$visit
 soma <- soma %>% filter(!(visit==2 & !is.na(DAYSTOTME) & DAYSTOTME<3652))
 # drop bariatric surgery variables
 soma <- soma %>% select(-c(TMETYPE,DAYSTOTME))
 
 # Save
-save(soma,file = "./Somalogic data raw/soma.Rdata")
-save(analytes,file = "./Somalogic data raw/analytes.Rdata")
+save(soma,file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/Somalogic data raw/soma.Rdata")
+save(analytes,file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/TODAY subaward/Somalogic data raw/analytes.Rdata")
