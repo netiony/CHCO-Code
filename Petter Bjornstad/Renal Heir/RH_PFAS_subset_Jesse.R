37b71272be33b515b1b230a51d198d24633b381e
Ye Ji Choi <yeji.choi@cuanschutz.edu>
2024-02-28 11:00:41 -0700

    Additional study pull for Jesse PFAS

--- Petter Bjornstad/Renal Heir/RH_PFAS_subset_Jesse.R
+++ Petter Bjornstad/Renal Heir/RH_PFAS_subset_Jesse.R
@@ -8,11 +8,20 @@ vars <- readxl::read_excel("/Volumes/Peds Endo/Petter Bjornstad/Renal HEIR/Data_
   filter(!is.na(...1))
 vars <- as.vector(vars)[[1]]
 
+ids <- read_xlsx("/Volumes/Peds Endo/Petter Bjornstad/RENAL HEIR/Data_Cleaned/Copy of phenotype_t_pfas_u_prot.xlsx",
+                 range = "B1:B150")
+
 pfas_subset <- dat %>%
-  filter(study == "RENAL-HEIR") %>%
-  dplyr::select(c(record_id, group, vars)) %>%
+  filter(study == "RENAL-HEIR" | study == "RENAL-HEIRitage" | study == "IMPROVE" | study == "CROCODILE") %>%
+  dplyr::select(c(record_id, visit, study, group, vars)) %>%
   dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, last(na.omit(.x)))),
                    across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, mean(.x, na.rm = TRUE))),
-                   .by = c(record_id)) 
+                   .by = c(record_id, visit)) %>%
+  filter(visit != "3_months_post_surgery") %>%
+  dplyr::mutate(record_id = case_when(visit == "12_months_post_surgery" ~ paste0(record_id, "_12M"),
+                                      visit == "baseline" & study == "IMPROVE" ~ paste0(record_id, "_BL"),
+                                      T ~ record_id)) %>%
+  dplyr::select(-c(study, visit)) %>%
+  filter(record_id %in% ids$`Phenotype IDs`)
 
 write.csv(pfas_subset, "/Volumes/Peds Endo/Petter Bjornstad/Renal HEIR/Data_Cleaned/RH PFAS subset.csv", row.names = F)
