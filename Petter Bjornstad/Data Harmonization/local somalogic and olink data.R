fa54d58a6831603af2384199b594d1ac90d1c829
laurapyle <laurapyle@gmail.com>
2024-03-01 14:41:31 -0700

    combined not harmonized

--- Petter Bjornstad/Data Harmonization/local somalogic and olink data.R
+++ Petter Bjornstad/Data Harmonization/local somalogic and olink data.R
@@ -73,168 +73,49 @@ soma <- soma %>%  mutate(
   )
 )
 
-soma_harmonized <- soma 
+soma_combined <- soma 
 
 # read in harmonized dataset to get group information
-df <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Data Clean/harmonized_dataset.csv", 
-               na.strings = c(" ", "", "-9999",-9999))
-coenroll_id <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Renal HERITAGE/Data_Cleaned/coenrolled_ids.csv") %>%
-  pivot_longer(cols = 'improve_id':'crc_id',
-               values_to = "record_id") %>% 
-  dplyr::select(merged_id, record_id) %>%
-  filter(record_id != "")
-df <- df %>%
-  dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, last(na.omit(.x)))),
-                   across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, mean(.x, na.rm = TRUE))),
-                   .by = c(record_id, visit)) %>%
-  filter(participation_status!="Removed"|is.na(participation_status)) %>%
-  left_join(coenroll_id)
-df <- df %>% 
-  dplyr::select(record_id, co_enroll_id, merged_id, study, visit, date, group)
-# Merge
-soma_harmonized$record_id <- soma_harmonized$SampleDescription
-soma_harmonized <- soma_harmonized %>% mutate(
-  record_id = case_when(
-    str_detect(record_id, "IT2D") ~ str_replace(record_id, "IT2D-", "IT_"),
-    !str_detect(record_id, "IT2D") ~ record_id
-  )
-)
-soma_harmonized <- soma_harmonized %>%  mutate(
-  visit = case_when(
-    TimePoint == "BL" ~ "baseline",
-    TimePoint == "Baseline" ~ "baseline",
-    TimePoint == "3M" ~ "3_months_post_surgery",
-    TimePoint == "12M" ~ "12_months_post_surgery"
-  )
-)
-soma_harmonized <- left_join(soma_harmonized, df, by = c("record_id", "visit"))
+# df <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Data Clean/harmonized_dataset.csv", 
+#                na.strings = c(" ", "", "-9999",-9999))
+# coenroll_id <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Renal HERITAGE/Data_Cleaned/coenrolled_ids.csv") %>%
+#   pivot_longer(cols = 'improve_id':'crc_id',
+#                values_to = "record_id") %>% 
+#   dplyr::select(merged_id, record_id) %>%
+#   filter(record_id != "")
+# df <- df %>%
+#   dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, last(na.omit(.x)))),
+#                    across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, mean(.x, na.rm = TRUE))),
+#                    .by = c(record_id, visit)) %>%
+#   filter(participation_status!="Removed"|is.na(participation_status)) %>%
+#   left_join(coenroll_id)
+# df <- df %>% 
+#   dplyr::select(record_id, co_enroll_id, merged_id, study, visit, date, group)
+# # Merge
+# soma_harmonized$record_id <- soma_harmonized$SampleDescription
+# soma_harmonized <- soma_harmonized %>% mutate(
+#   record_id = case_when(
+#     str_detect(record_id, "IT2D") ~ str_replace(record_id, "IT2D-", "IT_"),
+#     !str_detect(record_id, "IT2D") ~ record_id
+#   )
+# )
+# soma_harmonized <- soma_harmonized %>%  mutate(
+#   visit = case_when(
+#     TimePoint == "BL" ~ "baseline",
+#     TimePoint == "Baseline" ~ "baseline",
+#     TimePoint == "3M" ~ "3_months_post_surgery",
+#     TimePoint == "12M" ~ "12_months_post_surgery"
+#   )
+# )
+# soma_harmonized <- left_join(soma_harmonized, df, by = c("record_id", "visit"))
 
 # some extra spaces in the sample IDs
-soma_harmonized$SampleDescription <- str_trim(soma_harmonized$SampleDescription)
+soma_combined$SampleDescription <- str_trim(soma_combined$SampleDescription)
 # add labels
-labels <- paste0("log(",analytes$EntrezGeneSymbol[match(colnames(soma_harmonized), analytes$AptName)],")")
+labels <- paste0("log(",analytes$EntrezGeneSymbol[match(colnames(soma_combined), analytes$AptName)],")")
 labels[labels == "log(NA)"] <- ""
-label(soma_harmonized) <- as.list(labels)
+label(soma_combined) <- as.list(labels)
 
 # write copy of the entire local SomaScan dataframe to the data harmonization folder
-save(soma_harmonized, file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Harmonized SomaScan/soma_harmonized_anml.RData")
-save(analytes, file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Harmonized SomaScan/analytes.RData")
-
-###########################
-# MEDIAN NORMALIZED FILES #
-###########################
-
-# remove old data objects
-soma <- NULL
-soma2 <- NULL
-panther <- NULL
-soma_harmonized <- NULL
-
-# read in data 
-soma <- read.delim("./Local cohort Somalogic data/WUS-22-002/log2RFU_WUS-22-002_v4.1_EDTAPlasma.hybNorm.medNormInt.plateScale.calibrate.anmlQC.qcCheck.anmlSMP_quantile.txt")
-
-## STOPPED HERE - COLNAMES ARE ODD AND DECIDED TO ASK KATERINA ABOUT NORMALIZATIN
-
-soma <- soma %>% select(-Optional2)
-soma <- soma %>% filter(!is.na(SampleDescription))
-analytes <- getAnalyteInfo(soma)
-analytes <- analytes %>% select(AptName,SeqId,SeqIdVersion,SomaId,TargetFullName,Target,UniProt,EntrezGeneID,EntrezGeneSymbol,Organism,Units,Type)
-# remove fc mouse and no protein
-drop <- analytes %>% filter(Target == "Fc_MOUSE" | Target == "No Protein" | !(Organism == "Human") | !(Type == "Protein"))
-apt_drop <- drop$AptName
-soma <- soma %>% select(!all_of(apt_drop))
-analytes <- analytes %>% filter(!Target == "Fc_MOUSE")
-analytes <- analytes %>% filter(!Target == "No Protein")
-analytes <- analytes %>% filter(Organism == "Human")
-analytes <- analytes %>% filter(Type == "Protein")
-
-# read in 2nd dataset
-soma2 <- read_adat("./Local cohort Somalogic data/WUS-23-004/WUS_23_004_2023-11-15/WUS_23_004_v4.1_EDTAPlasma.hybNorm_medNormInt_plateScale_calibrate_anmlQC_qcCheck_anmlSMP.adat")
-soma2 <- soma2 %>% filter(!is.na(SampleDescription))
-# delete extraneous stuff in the sample description
-soma2$SampleDescription <- str_sub(soma2$SampleDescription, 12)
-soma2$SampleDescription <- str_remove(soma2$SampleDescription, " ")
-soma2$SampleDescription <- str_remove_all(soma2$SampleDescription, "\\(\\S+")
-# there is one duplicate sample - delete the second result
-soma2 <- soma2 %>% filter(!SampleDescription=="RH-14-O")
-# remove FC_Mouse and no protein
-soma2 <- soma2 %>% select(!all_of(apt_drop))
-analytes2 <- getAnalyteInfo(soma2)
-# 2nd analytes file is esssentially the same as the first except for some batch specific information we don't need
-# will keep the first file
-
-# read in PANTHER data
-panther <- read_adat("./Local cohort Somalogic data/PANTHER/20240126_597_Bjornstad_SOMAscan7k_WUS-24-002_data_export/WUS-24-002_2024-01-26_Somalogic_standardized_files/WUS_24_002_v4.1_EDTAPlasma.hybNorm.medNormInt.plateScale.calibrate.anmlQC.qcCheck.anmlSMP.adat")
-panther <- panther %>% filter(!is.na(SampleDescription))
-panther <- panther %>% select(-Optional1)
-# remove fc mouse and no protein
-panther <- panther %>% select(!all_of(apt_drop))
-# one ID changed
-panther$SampleDescription <- ifelse(panther$SampleDescription == "PAN-14-C", "PAN-14-O", panther$SampleDescription)
-
-# filter out Q/C samples
-soma <- rbind(soma,soma2,panther)
-# delete Pima data
-soma <- soma %>% filter(!str_detect(SampleDescription,"CKDS"))
-# fix sample IDs on a few RH2 participants who changed groups
-soma <- soma %>%  mutate(
-  SampleDescription = case_when(
-    SampleDescription == "RH2-13-T" ~ "RH2-13-O",
-    SampleDescription == "RH2-06-T" ~ "RH2-06-O",
-    SampleDescription == "RH2-07-T" ~ "RH2-07-O",
-    SampleDescription == "RH2-38-O" ~ "RH2-38-T",
-    .default = SampleDescription
-  )
-)
-
-soma_harmonized <- soma 
-
-# read in harmonized dataset to get group information
-df <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Data Clean/harmonized_dataset.csv", 
-               na.strings = c(" ", "", "-9999",-9999))
-coenroll_id <- read.csv("/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Renal HERITAGE/Data_Cleaned/coenrolled_ids.csv") %>%
-  pivot_longer(cols = 'improve_id':'crc_id',
-               values_to = "record_id") %>% 
-  dplyr::select(merged_id, record_id) %>%
-  filter(record_id != "")
-df <- df %>%
-  dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, last(na.omit(.x)))),
-                   across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, mean(.x, na.rm = TRUE))),
-                   .by = c(record_id, visit)) %>%
-  filter(participation_status!="Removed"|is.na(participation_status)) %>%
-  left_join(coenroll_id)
-df <- df %>% 
-  dplyr::select(record_id, co_enroll_id, merged_id, study, visit, date, group)
-# Merge
-soma_harmonized$record_id <- soma_harmonized$SampleDescription
-soma_harmonized <- soma_harmonized %>% mutate(
-  record_id = case_when(
-    str_detect(record_id, "IT2D") ~ str_replace(record_id, "IT2D-", "IT_"),
-    !str_detect(record_id, "IT2D") ~ record_id
-  )
-)
-soma_harmonized <- soma_harmonized %>%  mutate(
-  visit = case_when(
-    TimePoint == "BL" ~ "baseline",
-    TimePoint == "Baseline" ~ "baseline",
-    TimePoint == "3M" ~ "3_months_post_surgery",
-    TimePoint == "12M" ~ "12_months_post_surgery"
-  )
-)
-soma_harmonized <- left_join(soma_harmonized, df, by = c("record_id", "visit"))
-
-# some extra spaces in the sample IDs
-soma_harmonized$SampleDescription <- str_trim(soma_harmonized$SampleDescription)
-# add labels
-labels <- paste0("log(",analytes$EntrezGeneSymbol[match(colnames(soma_harmonized), analytes$AptName)],")")
-labels[labels == "log(NA)"] <- ""
-label(soma_harmonized) <- as.list(labels)
-
-# write copy of the entire local SomaScan dataframe to the data harmonization folder
-save(soma_harmonized, file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Harmonized SomaScan/soma_harmonized_anml.RData")
-save(analytes, file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Harmonized SomaScan/analytes.RData")
-
-
-
-
-
+save(soma_combined, file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Combined SomaScan/soma_combined_anml.RData")
+save(analytes, file = "/Volumes/Shared/Shared Projects/Laura/Peds Endo/Petter Bjornstad/Data Harmonization/Combined SomaScan/analytes.RData")
