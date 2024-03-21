d6096a3c68845a5cb17470f8557f5523ed83aecd
laurapyle <laurapyle@gmail.com>
2024-03-21 11:47:12 -0600

    add biopsy strata

--- Petter Bjornstad/KTR/create_randomization.R
+++ Petter Bjornstad/KTR/create_randomization.R
@@ -6,12 +6,20 @@
 library(blockrand)
 
 set.seed(3654)
-male_moderate <- blockrand(n=80, id.prefix='MM', block.prefix='MM',stratum='Male_moderate_albuminuria', block.sizes = 1:4)
-male_severe <- blockrand(n=80, id.prefix='MS', block.prefix='MS',stratum='Male_severe_albuminuria', block.sizes = 1:4)
-female_moderate <- blockrand(n=80, id.prefix='FM', block.prefix='FM',stratum='Female_moderate_albuminuria', block.sizes = 1:4)
-female_severe <- blockrand(n=80, id.prefix='FS', block.prefix='FS',stratum='Female_severe_albuminuria', block.sizes = 1:4)
+male_moderate_biopsy <- blockrand(n=80, id.prefix='MMB', block.prefix='MMB',stratum='Male_moderate_albuminuria_biopsy', block.sizes = 1:4)
+male_moderate_no_biopsy <- blockrand(n=80, id.prefix='MMN', block.prefix='MMN',stratum='Male_moderate_albuminuria_no_biopsy', block.sizes = 1:4)
 
-rand <- rbind(male_severe, male_moderate, female_severe, female_moderate)
+male_severe_biopsy <- blockrand(n=80, id.prefix='MSB', block.prefix='MSB',stratum='Male_severe_albuminuria_biopsy', block.sizes = 1:4)
+male_severe_no_biopsy <- blockrand(n=80, id.prefix='MSN', block.prefix='MSN',stratum='Male_severe_albuminuria_no_biopsy', block.sizes = 1:4)
+
+female_moderate_biopsy <- blockrand(n=80, id.prefix='FMB', block.prefix='FMB',stratum='Female_moderate_albuminuria_biopsy', block.sizes = 1:4)
+female_moderate_no_biopsy <- blockrand(n=80, id.prefix='FMN', block.prefix='FMN',stratum='Female_moderate_albuminuria_no_biopsy', block.sizes = 1:4)
+
+female_severe_biopsy <- blockrand(n=80, id.prefix='FSB', block.prefix='FSB',stratum='Female_severe_albuminuria_biopsy', block.sizes = 1:4)
+female_severe_no_biopsy <- blockrand(n=80, id.prefix='FSN', block.prefix='FSN',stratum='Female_severe_albuminuria_no_biopsy', block.sizes = 1:4)
+
+rand <- rbind(male_severe_biopsy, male_severe_no_biopsy, male_moderate_biopsy, male_moderate_no_biopsy, 
+              female_severe_biopsy, female_severe_no_biopsy, female_moderate_biopsy, female_moderate_no_biopsy)
 rand$treatment_char <- ifelse(rand$treatment == "A", "Placebo", "Dapagliflozin")
 
-write.csv(rand, "/Volumes/pylell/Kendrick Bjornstad KTR trial/KTR trial 23-1360 randomization.csv", row.names = F)
+write.csv(rand, "/Volumes/pylell/Kendrick Bjornstad KTR trial/KTR trial 23-1360 randomization with biopsy.csv", row.names = F)
