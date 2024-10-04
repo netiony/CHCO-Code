6c1e69ab3c4096b5fb03d80fc0141665538d84ba
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-10-04 13:34:20 -0600

    CALICO aim 1 initial report finished

--- Melanie Green/CALICO/create_analysis_dataset.R
+++ Melanie Green/CALICO/create_analysis_dataset.R
@@ -1,5 +1,6 @@
 library(redcapAPI)
 library(tidyverse)
+library(Hmisc)
 home_dir <- switch(Sys.info()["sysname"],
   "Darwin" = "/Users/timvigers/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Vigers/BDC/Janet Snell-Bergeon/CALICO",
   "Windows" = "C:/Users/timvigers/OneDrive - The University of Colorado Denver/Vigers/BDC/Janet Snell-Bergeon/CALICO",
@@ -58,38 +59,72 @@ df <- exportRecordsTyped(rcon,
   ),
   na = list(number = isMissingSpecial, radio = isMissingSpecial)
 )
-# Fill down age at diagnosis column
+# Obesity categories
+df <- df %>%
+  mutate(Obesity = case_when(
+    !is.na(cv_bmi_percentile) & cv_bmi_percentile >= 95 ~ "Yes",
+    !is.na(cv_bmi_percentile) & cv_bmi_percentile < 95 ~ "No",
+    is.na(cv_bmi_percentile) & cv_bmi >= 30 ~ "Yes",
+    is.na(cv_bmi_percentile) & cv_bmi < 30 ~ "Yes",
+    .default = NA
+  ))
+df$Obesity <- factor(df$Obesity, levels = c("No", "Yes"))
+# Combined race column
+races <- c(
+  "Caucasian", "African American", "Asian", "Pacific Islander",
+  "American Indian or Alaska Native", "Other", "Unknown/Not recorded",
+  "Not applicable", "Other", "Premenarchal"
+)
+df$combined_race <- apply(
+  df[, paste0("race___", c(1:5, 60, "unk"))], 1,
+  function(r) {
+    w <- which(r == "Checked")
+    if (length(w) == 0) {
+      return(NA)
+    } else if (length(w) > 1) {
+      return("More than one")
+    } else {
+      return(races[w])
+    }
+  }
+)
+df$combined_race <- factor(df$combined_race)
+# Fill down demographic columns
 df <- df %>%
   group_by(record_number) %>%
-  fill(pcosdx_age)
+  fill(combined_race, ethnicity, pcosdx_age)
 # Age group
 df$age_group <- cut(df$pcosdx_age, c(-Inf, 15, Inf),
   right = F, labels = c("< 15 years", ">= 15 years")
 )
-# Obesity categories
 # Convert columns to numeric
 df$cv_a1c <- suppressWarnings(as.numeric(df$cv_a1c))
 # Fix/add labels
-attr(df$age_group, "label") <- "Age Group at Diagnosis"
-labels(df[, grep("___unk", colnames(df))]) <-
-  sub(
+label(df$ethnicity) <- "Ethnicity"
+label(df$pcosdx_age) <- "Age at time of PCOS diagnosis"
+label(df$age_group) <- "Age Group at Diagnosis"
+label(df$combined_race) <- "Race"
+label(df$Obesity) <- "Obesity Status"
+label(df$cv_a1c) <- "HbA1C"
+label(df[, grep("___unk", colnames(df))]) <-
+  as.list(sub(
     "choice=NA", "choice=Unknown/Not recorded",
-    labels(df[, grep("___unk", colnames(df))])
-  )
-labels(df[, grep("___na", colnames(df))]) <-
-  sub(
+    label(df[, grep("___unk", colnames(df))])
+  ))
+label(df[, grep("___na", colnames(df))]) <-
+  as.list(sub(
     "choice=NA", "choice=Not applicable",
-    labels(df[, grep("___na", colnames(df))])
-  )
-labels(df[, grep("___oth", colnames(df))]) <-
-  sub(
+    label(df[, grep("___na", colnames(df))])
+  ))
+label(df[, grep("___oth", colnames(df))]) <-
+  as.list(sub(
     "choice=NA", "choice=Other",
-    labels(df[, grep("___oth", colnames(df))])
-  )
-labels(df[, grep("___pm", colnames(df))]) <-
-  sub(
+    label(df[, grep("___oth", colnames(df))])
+  ))
+label(df[, grep("___pm", colnames(df))]) <-
+  as.list(sub(
     "choice=NA", "choice=Premenarchal",
-    labels(df[, grep("___pm", colnames(df))])
-  )
+    label(df[, grep("___pm", colnames(df))])
+  ))
 # Save
 save(df, demo_vars, aim1_vars, file = "./Data_Clean/analysis_data.RData")
