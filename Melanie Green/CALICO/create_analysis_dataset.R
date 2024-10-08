69afd059c7afa92424157ea70394f79916099b74
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-10-08 17:58:40 -0600

    aim1 and SES updated

--- Melanie Green/CALICO/create_analysis_dataset.R
+++ Melanie Green/CALICO/create_analysis_dataset.R
@@ -46,6 +46,7 @@ demo_vars <- c(
 # Aim 1 for Melanie
 aim1_vars <- c(
   "cv_bmi", "cv_bmi_percentile", "cv_bmi_z",
+  "pcosdx_obesitydx_age",
   "cv_hirsutism_num", "cv_hirsutism_cat", "cv_acneface",
   "cv_acneother___1", "cv_acneother___2", "cv_acneother___0",
   "cv_acneother___unk", "cv_ft", "cv_ft_perc",
@@ -67,7 +68,7 @@ aim1_vars <- c(
   "cv_medications___25", "cv_medications___26", "cv_medications___27",
   "cv_medications___28", "cv_medications___29", "cv_medications___30",
   "cv_medications___31", "cv_medications___60", "cv_medications___0",
-  "cv_medications___unk"
+  "cv_medications___unk", "pcosdx_specialty"
 )
 # LARC for BCH
 larc_vars <- c(
@@ -79,12 +80,15 @@ larc_vars <- c(
   "cv_hirsutism_num", "cv_hirsutism_cat", "cv_menfreq", "race___1", "race___2",
   "race___3", "race___4", "race___5", "race___60", "race___unk",
   "ethnicity", "insur_type", "cv_genderid",
-  "cv_genderid_other", "cv_sexuallyactive", "cv_sexualpref",
+  "cv_sexuallyactive", "cv_sexualpref",
   "cv_monthssincepcosdx", "cv_hgb", "cv_ldl", "cv_hdl", "cv_sbp", "cv_dbp"
 )
 # SES variables for health disparities
 ses_vars <- c(
-  "pcosdx_birthweight", "pcosdx_birthweight_oz", "pcosdx_birthweight_calc",
+  "pcosdx_birthweight_calc", "cv_bmi",
+  "cv_bmi_percentile", "cv_bmi_z", "cv_a1c", "cv_tt_perc", "cv_ft_perc",
+  "cv_shbg", "cv_fbg", "cv_2hrglucoseogtt", "cv_tg_fasting", "cv_tg", "cv_hdl",
+  "cv_ldl", "cv_tc", "cv_alt", "cv_ast",
   "pcosdx_pmh___1", "pcosdx_pmh___2", "pcosdx_pmh___3", "pcosdx_pmh___4",
   "pcosdx_pmh___5", "pcosdx_pmh___6", "pcosdx_pmh___29", "pcosdx_pmh___7",
   "pcosdx_pmh___8", "pcosdx_pmh___9", "pcosdx_pmh___10", "pcosdx_pmh___11",
@@ -92,7 +96,6 @@ ses_vars <- c(
   "pcosdx_pmh___15", "pcosdx_pmh___16", "pcosdx_pmh___23", "pcosdx_pmh___24",
   "pcosdx_pmh___17", "pcosdx_pmh___18", "pcosdx_pmh___19", "pcosdx_pmh___20",
   "pcosdx_pmh___21", "pcosdx_pmh___60", "pcosdx_pmh___0", "pcosdx_pmh___unk",
-  "pmh_other",
   "pcosdx_famhx_parent___27", "pcosdx_famhx_parent___28",
   "pcosdx_famhx_parent___30", "pcosdx_famhx_parent___5",
   "pcosdx_famhx_parent___29", "pcosdx_famhx_parent___7",
@@ -119,12 +122,11 @@ ses_vars <- c(
   "pastmeds___8", "pastmeds___9", "pastmeds___10", "pastmeds___11",
   "pastmeds___12", "pastmeds___13", "pastmeds___14", "pastmeds___15",
   "pastmeds___16", "pastmeds___60", "pastmeds___0", "pastmeds___unk",
-  "pastmeds_other",
-  "pcosdx_medcomments", "pcosdx_mentalhealthcounseling___1",
+  "pcosdx_mentalhealthcounseling___1",
   "pcosdx_mentalhealthcounseling___2", "pcosdx_mentalhealthcounseling___3",
   "pcosdx_mentalhealthcounseling___0", "pcosdx_mentalhealthcounseling___unk",
   "pcosdx_age", "pcosdx_specialty",
-  "pcosdx_specialty_other", "cv_visittype", "cv_type_of_clinic", "clinic_other",
+  "cv_visittype", "cv_type_of_clinic",
   "cv_mdc_specialties___1", "cv_mdc_specialties___2", "cv_mdc_specialties___3",
   "cv_mdc_specialties___4", "cv_mdc_specialties___5", "cv_mdc_specialties___6",
   "cv_mdc_specialties___60",
@@ -149,10 +151,7 @@ ses_vars <- c(
   "cv_medications___26", "cv_medications___27", "cv_medications___28",
   "cv_medications___29", "cv_medications___30", "cv_medications___31",
   "cv_medications___60", "cv_medications___0", "cv_medications___unk",
-  "cv_medscomments", "cv_othercomment", "cv_hormonecomments", "cv_bmi",
-  "cv_bmi_percentile", "cv_bmi_z", "cv_a1c", "cv_tt_perc", "cv_ft_perc",
-  "cv_shbg", "cv_fbg", "cv_2hrglucoseogtt", "cv_tg_fasting", "cv_tg", "cv_hdl",
-  "cv_ldl", "cv_tc", "cv_alt", "cv_ast", "cv_newmeds___1", "cv_newmeds___2",
+  "cv_newmeds___1", "cv_newmeds___2",
   "cv_newmeds___3", "cv_newmeds___4", "cv_newmeds___5", "cv_newmeds___6",
   "cv_newmeds___7", "cv_newmeds___8", "cv_newmeds___9", "cv_newmeds___10",
   "cv_newmeds___11", "cv_newmeds___12", "cv_newmeds___13", "cv_newmeds___14",
@@ -161,14 +160,12 @@ ses_vars <- c(
   "cv_newmeds___23", "cv_newmeds___32", "cv_newmeds___24", "cv_newmeds___25",
   "cv_newmeds___26", "cv_newmeds___27", "cv_newmeds___28", "cv_newmeds___29",
   "cv_newmeds___30", "cv_newmeds___31", "cv_newmeds___60",
-  "cv_newcont_meds_othercomment", "cv_newcont_meds_hormonecomments",
-  "cv_newcont_meds_discontinued", "cv_referrals___1", "cv_referrals___2",
+  "cv_referrals___1", "cv_referrals___2",
   "cv_referrals___3", "cv_referrals___4", "cv_referrals___5",
   "cv_referrals___6", "cv_referrals___7", "cv_referrals___8",
   "cv_referrals___9", "cv_referrals___10", "cv_referrals___11",
   "cv_referrals___12", "cv_referrals___60",
-  "cv_referrals___unk",
-  "cv_referrals_other", "cv_referrals_reason"
+  "cv_referrals___unk"
 )
 # Obesity categories
 df <- df %>%
@@ -204,6 +201,19 @@ df$combined_race <- apply(
   }
 )
 df$combined_race <- factor(df$combined_race)
+# Mental health screening
+df$mental_health_screening <-
+  rowSums(!is.na(df[, c("cv_phq2", "cv_phq8", "cv_phq9", "cv_cesd20")]))
+df$mental_health_screening <- factor(df$mental_health_screening,
+  levels = 0:1, labels = c("No", "Yes")
+)
+# Region
+df$Region <- df$site
+levels(df$Region) <- c(
+  "West", "Northeast", "Southeast", "Midwest", "West", "Midwest", "Northeast",
+  "Midwest", "Northeast", "Southeast", "West", "Southeast", "Southeast",
+  "Midwest", "Southeast", "Midwest", "Southwest", "Southwest"
+)
 # LARC
 df$larc <- df$cv_medications___10 == "Checked" |
   df$cv_medications___11 == "Checked"
@@ -213,11 +223,6 @@ larc_users <- unique(df$record_number[df$larc == "Yes"])
 df$larc_ever <- "No"
 df$larc_ever[df$record_number %in% larc_users] <- "Yes"
 df$larc_ever <- factor(df$larc_ever, levels = c("No", "Yes"))
-# Fill down select columns
-df <- df %>%
-  group_by(record_number) %>%
-  fill(combined_race, ethnicity, pcosdx_age, larc_ever, insur_type) %>%
-  ungroup()
 # Age group at diagnosis
 df$age_group <- cut(df$pcosdx_age, c(-Inf, 15, Inf),
   right = F, labels = c("< 15 years", ">= 15 years")
@@ -227,6 +232,7 @@ df$cv_a1c <- suppressWarnings(as.numeric(df$cv_a1c))
 df$redcap_repeat_instance <- as.numeric(df$redcap_repeat_instance)
 # Fix/add labels
 label(df$insur_type) <- "Insurance Type"
+label(df$Region) <- "Region"
 label(df$ethnicity) <- "Ethnicity"
 label(df$pcosdx_age) <- "Age at time of PCOS diagnosis"
 label(df$age_group) <- "Age Group at Diagnosis"
@@ -236,6 +242,7 @@ label(df$Obesity_raw) <- "Obesity Status (by raw value)"
 label(df$cv_a1c) <- "HbA1C"
 label(df$larc) <- "On LARC"
 label(df$larc_ever) <- "LARC Ever User"
+label(df$mental_health_screening) <- "PHQ-2, PHQ-8, PHQ-9 or CED-S Score Available?"
 label(df[, grep("___unk", colnames(df))]) <-
   as.list(sub(
     "choice=NA", "choice=Unknown/Not recorded",
