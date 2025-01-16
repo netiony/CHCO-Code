c2e5a057f088665621a44fa229bb71abc2f218fa
choiyej12 <yeji.choi@cuanschutz.edu>
2025-01-15 17:04:23 -0700

    RH additional data pull for Jesse and Hongxu

--- Petter Bjornstad/Renal Heir/RH_PFAS_subset_Jesse.R
+++ Petter Bjornstad/Renal Heir/RH_PFAS_subset_Jesse.R
@@ -13,14 +13,16 @@ ids <- read_xlsx("/Volumes/Peds Endo/Petter Bjornstad/RENAL HEIR/Data_Cleaned/Co
 
 pfas_subset <- dat %>%
   filter(study == "RENAL-HEIR" | study == "RENAL-HEIRitage" | study == "IMPROVE" | study == "CROCODILE") %>%
-  dplyr::select(c(record_id, visit, study, group, vars)) %>%
+  dplyr::select(c(record_id, visit, study, group, vars, eGFR_fas_cr,
+                  di, gir_190)) %>%
   dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, last(na.omit(.x)))),
                    across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, mean(.x, na.rm = TRUE))),
                    .by = c(record_id, visit)) %>%
   filter(visit != "3_months_post_surgery") %>%
   dplyr::mutate(record_id = case_when(visit == "12_months_post_surgery" ~ paste0(record_id, "_12M"),
                                       visit == "baseline" & study == "IMPROVE" ~ paste0(record_id, "_BL"),
-                                      T ~ record_id)) %>%
+                                      T ~ record_id),
+                m_i = gir_190/steady_state_insulin) %>%
   dplyr::select(-c(study, visit)) %>%
   filter(record_id %in% ids$`Phenotype IDs`)
 
