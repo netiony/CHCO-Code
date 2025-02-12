c564970d2409ce9f7eab7c7070870b5d5fcf5116
hehampson <hhampson@usc.edu>
2025-02-11 16:17:35 -0800

    install seurat disk from github

--- Petter Bjornstad/Kidney scRNA/Kidney scRNA/Foramt_seurat_for_Jenny.R
+++ Petter Bjornstad/Kidney scRNA/Kidney scRNA/Foramt_seurat_for_Jenny.R
@@ -1,3 +1,13 @@
 #Load in seurat .h5seurat object& convert to .rds
 install.packages("Seurat")
-install.packages("SeuratDisk")
+install.packages("remotes")
+remotes::install_github("mojaveazure/seurat-disk",force=T)
+
+library(SeuratDisk)
+library(Seurat)
+
+# Load your .h5seurat file
+seurat_obj <- LoadH5Seurat("/Users/hhampson/Downloads/KPMP.h5Seurat")
+
+# Save as .RDS
+saveRDS(seurat_obj, file = "/Users/hhampson/Downloads/KPMP.rds")
