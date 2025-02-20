d37598382ad2f81039fab96043759d7856ae3a6d
Hailey <hhampson@usc.edu>
2025-02-20 11:08:52 -0800

    add study visit to decode harm data

--- Petter Bjornstad/DECODE/DECODE_biopsy_harmonization.R
+++ Petter Bjornstad/DECODE/DECODE_biopsy_harmonization.R
@@ -9,8 +9,8 @@ colnames(bmt) <- bmt[2,]
 bmt <- bmt[-c(1,2),]
 
 bmt_sum <- bmt %>% 
-  group_by(Study, `Shipped Y/N`, `scRNA status`) %>%
+  group_by(Study, `Shipped Y/N`, `scRNA status`,`Visit ID`) %>%
   summarise(Count = n()) %>%
   ungroup
 
-write.csv(bmt_sum,fs::path(dir.results,"Biopsies and Sequencing","Biopsies_Sequencing_by_Study.csv"))
+write.csv(bmt_sum,fs::path(dir.results,"Biopsies and Sequencing","Biopsies_Sequencing_by_Study_Visit.csv"))
